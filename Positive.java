b06efb2ed97f831719d2f69dc8f2eee59180ef08
MuthuSubalakshmi <100410577+MuthuSubalakshmi@users.noreply.github.com>
2022-02-28 22:20:20 +0530

    Add files via upload

---
+++ Positive.java
@@ -0,0 +1,18 @@
+package week1.pgm2;
+
+public class Positive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int number=-40;
+ if(number<0)
+ {
+	 System.out.println("The Number "+ number +"is converted to"+ (-number));
+ }
+ else
+ {
+	 System.out.println("The number is positive");
+ }
+	}
+
+}
